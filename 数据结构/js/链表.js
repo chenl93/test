1eacf93d62cd4db1ed3fd5e3d3272842dac5230c
chenl1988 <572117292@qq.com>
2019-05-11 11:04:46 +0800

    链表

--- 数据结构/js/链表.js
+++ 数据结构/js/链表.js
@@ -12,13 +12,18 @@ class LinkedList{
   constructor(){
     this.head = null;
   }
-
+  /* 在某一个元素后面插入 */
   insert(value,after){
     const nodeInsert = new Node(value);
     if(!this.head){
       this.head = nodeInsert;
       return;
     }
+    const node = this._findNode(after);
+    /* 将找到元素的下一个节点保存下来，插入后将保存下来的下一个节点值保存给插入的节点 */
+    const orginalNext = node.next;
+    node.next = nodeInsert;
+    nodeInsert.next = orginalNext;
   }
   _findNode(value){
     if(!this.head) return null; //如果链表没有 头 直接返回null
