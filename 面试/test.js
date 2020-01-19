eedb19e4d7dd6536e1cdbea561dbfdaebdc64fb5
chenl93 <572117292@qq.com>
2020-01-19 12:57:49 +0800

    20200119

--- 面试/test.js
+++ 面试/test.js
@@ -1,4 +1,4 @@
-let arr = [33, 21, 9, 23, 14, 55, 75, 4, 63, 12, 77];
+/* let arr = [33, 21, 9, 23, 14, 55, 75, 4, 63, 12, 77];
 
 
 function quickSort(arr){
@@ -22,4 +22,33 @@ function quickSort(arr){
   return quickSort(left).concat(midVal,quickSort(right));
 }
 
-console.log(quickSort(arr));
+console.log(quickSort(arr)); */
+
+function test(person){
+  person.age = 26;
+  person = {
+    name:'b',
+    age:18
+  }
+  return person;
+}
+
+const p1 = {
+  name:'a',
+  age:19
+}
+
+const p2 = test(p1);
+console.log(p1);
+console.log(p2);
+
+/* 
+  函数传参的时候是对象在堆中的内存地址值 ，
+  test函数中的实参person是p1对象的内存地址，通过调用person.age = 26确实改变了p1值
+  但随后person变成了另一块内存空间的地址，并且在最后将这另外一份内存地址返回，赋值给了p2
+
+
+  重点： 函数中参数的类型是复杂数据类型时，传递的是内存地址
+  当参数被=号重新赋值时，会产生一个新的内存地址
+
+*/
